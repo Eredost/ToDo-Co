e98da12919bfb6f0bd81aee1be30437fb4d69cec
Eredost <michael.coutin62@gmail.com>
2021-04-14 20:27:23 +0200

    feat: upgrade Symfony version from 4.0 to 4.4

--- src/Repository/TaskRepository.php
+++ src/Repository/TaskRepository.php
@@ -5,7 +5,7 @@ namespace App\Repository;
 
 use App\Entity\Task;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method Task|null find($id, $lockMode = null, $lockVersion = null)
